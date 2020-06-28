5cfaea9b9778da68a4a9f91d37de98fcf15f3168
engineertree5 <engineertree5@gmail.com>
2020-06-28 13:02:11 -0400

    updating files

--- pimation/moneycall.py
+++ pimation/moneycall.py
@@ -4,7 +4,14 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 from random import sample
 import csv
-from datetime import date
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+from mpl_finance import candlestick_ohlc
+import pandas as pd
+import pandas_datareader.data as web
+import matplotlib.dates as mdates
+from IPython.core.pylabtools import figsize
 ###
 import tweepy
 from secrets import *
@@ -18,13 +25,59 @@ user = api.get_user('lordfili')
 
 
 # GLOBAL VARS
-today = date.today()
-d_slash = today.strftime("%Y/%m/%d")
-d_dash = today.strftime("%Y-%m-%d")
 watchlist = ['MSFT', 'V', 'DLR', 'CONE', 'PING', 'AMD', 'O', 'BAM', 'DDOG', 'ADBE', 'NKE', 'CHWY', 'NOK', 'BIP', 'O', 'DOCU', 'QCOM', 'BABA', 'DIS', 'ZS', 'NVDA', 'CCI', 'AMT', 'RTX']
 chart_dir = '/home/pi/Documents/automation/awtybot/'
 days = 365 # Stock data to chart against
 
+def candle_picks():
+    figsize(14, 7)
+    style.use('ggplot')
+    stock_list = sample(watchlist, 4)
+    print(f'random stock pick from watchlist ${stock_list}')
+    #set current date
+    today = dt.datetime.now().date()
+    end = dt.datetime(today.year, today.month,today.day)
+    start = dt.datetime(today.year -1, today.month,today.day)
+    pick_info = {}
+    pick_info['symbols'] = {}
+    for stock_pick in stock_list:
+        company_symbol = yf.Ticker(f'{stock_pick}')
+        data = company_symbol.history(period=f"{days}d")
+        try:
+            s_name = company_symbol.get_info()['shortName']
+        except IndexError as err:
+            s_name = '*'
+            print(err)
+        pick_info['symbols'][stock_pick] = s_name
+        
+        df = web.DataReader(f'{stock_pick}', 'yahoo', start=start, end=end)
+        df.to_csv(f'{stock_pick}.csv')
+        df = pd.read_csv(f'{stock_pick}.csv', parse_dates=True, index_col=0)
+        
+
+        df['200d_EMA'] = df.Close.ewm(span=200,min_periods=0,adjust=False,ignore_na=False).mean()
+        df['50d_EMA'] = df.Close.ewm(span=50,min_periods=0,adjust=False,ignore_na=False).mean()     
+
+        df_ohlc = df['Adj Close'].resample('10D').ohlc()
+        df_volume = df['Volume'].resample('10D').sum()
+
+        df_ohlc.reset_index(inplace=True)
+        df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+        ax1 = plt.subplot2grid((6,1), (0,0), rowspan=4, colspan=1, title=f"${stock_pick} STOCK")
+        ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1, label='Volume')
+        # Plt.subplot2grid(shape, location, rowspan, colspan)
+
+        candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+        ax1.plot(df.index, df[['200d_EMA']], label='200d_EMA')
+        ax1.plot(df.index, df[['50d_EMA']], label='50d_EMA')
+        ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0) #x and y 
+        ax1.xaxis_date()
+        ax1.legend()
+        # fig= plt.figure(num=1, figsize=(6,3))
+        plt.savefig(f'{chart_dir}{stock_pick}.png', bbox_inches='tight')
+        # plt.show()
+    return pick_info
 
 def random_picks():
     stock_list = sample(watchlist, 4)
@@ -54,13 +107,12 @@ def random_picks():
         # shortname = company_symbol.get_info()['shortName']
         
 
-        data['50d_EMA'] = data.Close.ewm(span=50,min_periods=0,adjust=False,ignore_na=False).mean()
-        data['20d_EMA'] = data.Close.ewm(span=20,min_periods=0,adjust=False,ignore_na=False).mean()
-        data['100d_EMA'] = data.Close.ewm(span=100,min_periods=0,adjust=False,ignore_na=False).mean()
-        
+        data['50d_SMA'] = data.Close.rolling(window=50).mean()
+        data['100d_SMA'] = data.Close.rolling(window=100).mean()
+        data['200d_SMA'] = data.Close.rolling(window=200).mean()
 
         fig, ax = plt.subplots()
-        data[['Close', '50d_EMA', '100d_EMA', '20d_EMA']].plot(title=f"${stock_pick} STOCK {d_dash}", figsize=(10,5), ax=ax)
+        data[['Close', '50d_SMA', '100d_SMA', '200d_SMA']].plot(title=f"${stock_pick} STOCK {d_dash}", figsize=(10,5), ax=ax)
         # Don't allow the axis to be on top of your data
         ax.set_axisbelow(True)
         ax.grid()
@@ -94,7 +146,7 @@ def update_status(pick_info):
         print(e.response.text)
 
 def main():
-    stock_list = random_picks()
+    stock_list = candle_picks()
     update_status(stock_list)
 
 if __name__ == "__main__":
